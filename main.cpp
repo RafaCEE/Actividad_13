033382f21441ce84403dd987f91c5bc132a5f20c
Rafael Cardona <rafael.cardona@alumnos.udg.mx>
2020-11-03 23:52:59 -0600

    Implementacion de clase arreglo y funciones

--- main.cpp
+++ main.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main()
 {
+    Arreglo<int> myarray;
+    for (size_t i = 0; i < 10; i++)
+    {
+        myarray.insertar_final(i);
+
+    }
+    myarray.insertar(100, 5);
+    myarray.eliminar(5);
+     for (size_t i = 0; i < 10; i++)
+    {
+        cout << myarray[i] << " ";
+
+    }
     
+
+    cout << endl;
+    
+
     
     return 0;
 }
