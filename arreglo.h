fc5f52c702cb32aaae42ee2dfb1e934f6a372e80
Rafael Cardona <rafael.cardona@alumnos.udg.mx>
2020-11-03 23:21:22 -0600

    Creacion de la clase arreglo

---
+++ arreglo.h
@@ -0,0 +1,26 @@
+#ifndef __ARREGLO_H__
+#define __ARREGLO_H__
+#include <iostream>
+
+class Arreglo
+{
+private:
+    /* data */
+public:
+    Arreglo(/* args */);
+    ~Arreglo();
+};
+
+Arreglo::Arreglo(/* args */)
+{
+}
+
+Arreglo::~Arreglo()
+{
+}
+
+
+
+
+
+#endif // __ARREGLO_H__
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "arreglo.h"
 using namespace std;
 
 int main()
