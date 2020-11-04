1a71846fd290532914b78ee732a7de10ff9db009
Rafael Cardona <rafael.cardona@alumnos.udg.mx>
2020-11-04 10:18:12 -0600

    Correccion en iteracion recorrer hasta

--- main.cpp
+++ main.cpp
@@ -12,26 +12,28 @@ int main()
     arreglo.insertar_inicio("Hola");
 
     // mostrar() los elementos de arreglo
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < arreglo.size(); i++)
     {
         cout << arreglo[i] << " ";
 
     }
+    cout << endl;
     // insertar() el elemento en la posición 2
     arreglo.insertar("Jesus", 2);
     // mostrar los elementos de arreglo
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < arreglo.size(); i++)
     {
         cout << arreglo[i] << " ";
 
     }
+    cout << endl;
     // eliminar_inicio() y eliminar_final()
     arreglo.eliminar_inicio();
     arreglo.eliminar_final();
     // eliminar() el elemento de la posición 1
     arreglo.eliminar(1);
     // mostrar() los elementos de arreglo
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < arreglo.size(); i++)
     {
         cout << arreglo[i] << " ";
 
