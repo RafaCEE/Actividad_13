7172144a177662b67c2f8d8256157e225788d0f4
Rafael Cardona <rafael.cardona@alumnos.udg.mx>
2020-11-03 23:59:32 -0600

    Tarea 13

--- main.cpp
+++ main.cpp
@@ -4,19 +4,55 @@ using namespace std;
 
 int main()
 {
-    Arreglo<int> myarray;
+    Arreglo<string> arreglo;
+    // agregar 4 string's a arreglo usando insertar_final e insersar_inicio()
+    arreglo.insertar_final("Como");
+    arreglo.insertar_inicio("Rafael");
+    arreglo.insertar_final("Estas?");
+    arreglo.insertar_inicio("Hola");
+
+    // mostrar() los elementos de arreglo
     for (size_t i = 0; i < 10; i++)
     {
-        myarray.insertar_final(i);
+        cout << arreglo[i] << " ";
 
     }
-    myarray.insertar(100, 5);
-    myarray.eliminar(5);
-     for (size_t i = 0; i < 10; i++)
+    // insertar() el elemento en la posición 2
+    arreglo.insertar("Jesus", 2);
+    // mostrar los elementos de arreglo
+    for (size_t i = 0; i < 10; i++)
     {
-        cout << myarray[i] << " ";
+        cout << arreglo[i] << " ";
 
     }
+    // eliminar_inicio() y eliminar_final()
+    arreglo.eliminar_inicio();
+    arreglo.eliminar_final();
+    // eliminar() el elemento de la posición 1
+    arreglo.eliminar(1);
+    // mostrar() los elementos de arreglo
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << arreglo[i] << " ";
+
+    }
+
+
+
+
+    // Arreglo<string> myarray;
+    // for (size_t i = 0; i < 10; i++)
+    // {
+    //     myarray.insertar_final("Hola");
+
+    // }
+    // myarray.insertar("Adios", 5);
+    // myarray.eliminar(5);
+    //  for (size_t i = 0; i < 10; i++)
+    // {
+    //     cout << myarray[i] << " ";
+
+    // }
     
 
     cout << endl;
